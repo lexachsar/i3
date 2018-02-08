981446340347c8edd34ce7d247dfd6918acd1a06
lexach <lexachsar@gmail.com>
2018-02-09 01:04:12 +0400

    new file:   .ToDo.todo 	modified:   config/config (moved theming to theme.conf file) 	modified:   install.sh (modified install.sh to generate i3 config with theme.conf) 	new file:   theme.conf

--- install.sh
+++ install.sh
@@ -4,25 +4,24 @@ apt install i3-wm
 apt install i3lock
 apt install i3blocks
 
+##############################
+# A directory for temp files #
+##############################
 mkdir tmp
-cd tmp
 
 
-############################################################################################################
-#installing dependencies
-########################
-########################
+###########################
+# installing dependencies #
+###########################
 
 #clonning repo with San Francisco fonts
-git clone https://github.com/supermarin/YosemiteSanFranciscoFont.git
+git clone https://github.com/supermarin/YosemiteSanFranciscoFont.git tmp/YosemiteSanFranciscoFont
 
-cd YosemiteSanFranciscoFont
 #making directory for fonts
 mkdir ~/.fonts
 #copying all fonts to fonts directory
-cp -v *.ttf ~/.fonts
+cp -r -v tmp/YosemiteSanFranciscoFont/*.ttf ~/.fonts
 
-cd ../
 
 #install rofi
 apt install rofi
@@ -45,19 +44,11 @@ add-apt-repository ppa:moka/daily
 apt-get update
 apt-get install moka-icon-theme faba-icon-theme faba-mono-icons
 
-cd ../
-rm -r -f -v tmp
+###########################
+# Config files generation #
+###########################
 
-#config files generation
-########################
-########################
-
-cd config
-mkdir tmpConfig
-
-cp config tmpConfig
-
-cd tmpConfig
+cp config/config tmp
 
 #screen resolution specifying
 {
@@ -72,10 +63,11 @@ cd tmpConfig
 	done    
 }
 
-echo "#screen settings" >> config
-echo "exec_always xrandr --output VGA-1 --primary --mode "$a"x"$b" --pos 0x0 --rotate normal --output DVI-I-1 --off --output HDMI-1 --off" >> config
+echo "#screen settings" >> tmp/config
+echo "exec_always xrandr --output VGA-1 --primary --mode "$a"x"$b" --pos 0x0 --rotate normal --output DVI-I-1 --off --output HDMI-1 --off" >> tmp/config
 
-cd ../
+#theme add
+echo | cat theme.conf >> tmp/config 
 
 #copying config files to their destenation places
 #################################################
@@ -83,34 +75,32 @@ cd ../
 
 #copyinf i3 windows manager config file
 mkdir ~/.config/i3
-cp -f -v tmpConfig/config ~/.config/i3
+cp -f -v tmp/config ~/.config/i3
 
 #copying i3 blocks config file
-cp -f -v i3blocks.conf ~/.config/i3
+cp -f -v config/i3blocks.conf ~/.config/i3
 
 #copying gtk-3.0 config file
 mkdir ~/.config/gtk-3.0
-cp -f -v settings.ini ~/.config/gtk-3.0
+cp -f -v config/settings.ini ~/.config/gtk-3.0
 
 #copying gtk-2.0 config file
-cp -f -v .gtkrc-2.0 ~/
-
-rm -f -v -r tmpConfig
-cd ../
+cp -f -v config/.gtkrc-2.0 ~/
 
 #copying scripts to their destinattion
-cd scripts
 
-cp -f -v lock.sh ~/.config/i3
+cp -f -v scripts/lock.sh ~/.config/i3
 
-cd ../
 
 #copying pictures
 
-cd pictures
 
 mkdir ~/Pictures/Icons
 
-cp -f -v lock.png ~/Pictures/Icons
+cp -f -v pictures/lock.png ~/Pictures/Icons
 
-cd ../
+####################
+# tmp dir removing #
+####################
+
+rm -r -f -v tmp 
